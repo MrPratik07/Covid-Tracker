d857e370b77e94fba5bacf0d63a73bdf9d6a9a9d
pratik <pratikgaikwad551@gmail.com>
2020-07-30 11:48:03 +0530

    Updates

---
+++ precache-manifest.8771ccadca772db1fdb780d0759d3758.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae816f620c04202bc18bdd332460080e",
+    "url": "/Covid-Tracker/index.html"
+  },
+  {
+    "revision": "9dff6460879cb37f058e",
+    "url": "/Covid-Tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "c56b87c0c31dbfe10df1",
+    "url": "/Covid-Tracker/static/css/main.57e5168c.chunk.css"
+  },
+  {
+    "revision": "9dff6460879cb37f058e",
+    "url": "/Covid-Tracker/static/js/2.95d21892.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid-Tracker/static/js/2.95d21892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c56b87c0c31dbfe10df1",
+    "url": "/Covid-Tracker/static/js/main.e81d0263.chunk.js"
+  },
+  {
+    "revision": "cffe3fea90f67a97869b",
+    "url": "/Covid-Tracker/static/js/runtime-main.eb8debe2.js"
+  }
+]);
